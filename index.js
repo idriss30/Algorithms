c25592a39cd46ba88401f7397ed5dbb390a39e1b
idriss30 <idriss.cissoko@hotmail.com>
2021-08-15 22:13:53 -0400

    sorted union algorithm

--- index.js
+++ index.js
@@ -296,10 +296,43 @@ if(str === alphabet){
 
 
 
-fearNotLetter("abce");
+/* fearNotLetter("abce");
 fearNotLetter("abcdefghjklmno")
 fearNotLetter("stvwx")
 console.log(fearNotLetter("abcdefghijklmnopqrstuvwxyz"))
 
 
+ */
+/* 
+Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
 
+In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+
+The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order. */
+
+
+function uniteUnique(arr) {
+
+let allArguments = Array.from(arguments)
+let firstArg = allArguments.shift();
+let secondsArg = []
+
+allArguments.forEach(element =>  {
+    element.filter(val => {
+        if(!firstArg.includes(val)){
+            secondsArg.push(val)
+        }
+    })
+})
+
+arr = firstArg.concat(secondsArg);
+
+
+return arr
+
+
+}
+
+uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1]);
+uniteUnique([1, 2, 3], [5, 2, 1]);
+uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])
