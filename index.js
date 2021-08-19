56e2dedecbca57e5f75eb5204b00bfa62fecfb72
idriss30 <idriss.cissoko@hotmail.com>
2021-08-19 10:32:45 -0400

    sum of primes number

--- index.js
+++ index.js
@@ -390,3 +390,48 @@ sumFibs(10)
 sumFibs(1);
 
  */
+
+
+//Sum All PrimesPassed
+/*A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is not prime since it is divisible by 1, 2 and 4.
+
+Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num. */
+
+
+
+
+
+
+let checkPrime = (num)=>{
+  if(num === 2 ) return true
+  let start = 2;
+  let final = Math.ceil(Math.sqrt(num));
+  while(start <= final){
+      if(num % start === 0) return 0;
+      start++  
+  }
+
+
+  return num > 1
+
+}
+
+
+function sumPrimes(num) {
+
+if( num === 1) return 1;
+let numbersArr = []
+for(let i  = 0; i <= num; i++){
+   if(checkPrime(i)){
+       numbersArr.push(i)
+   }
+}
+
+return numbersArr.reduce((a,b)=> a+b, 0)
+
+}
+
+sumPrimes(10);
+console.log('////////////////////')
+sumPrimes(30);
+
