022f46a01a1299c61da442cf185dd30daff1be06
idriss30 <idriss.cissoko@hotmail.com>
2021-08-21 08:00:51 -0400

    small commun multiple

--- index.js
+++ index.js
@@ -431,7 +431,41 @@ return numbersArr.reduce((a,b)=> a+b, 0)
 
 }
 
-sumPrimes(10);
+/* sumPrimes(10);
 console.log('////////////////////')
 sumPrimes(30);
+ */
 
+
+/*  Smallest Common Multiple
+Find the smallest common multiple of the provided parameters that can be evenly divided by both, as well as by all sequential numbers in the range between these parameters.
+
+The range will be an array of two numbers that will not necessarily be in numerical order.
+
+For example, if given 1 and 3, find the smallest common multiple of both 1 and 3 that is also evenly divisible by all numbers between 1 and 3. The answer here would be 6.     */
+
+
+
+function smallestCommons(arr) {
+
+ var max = Math.max(arr[0], arr[1]);
+  var min = Math.min(arr[0], arr[1]);
+  let multiple = max;
+  for (let i = max; i >= min; i--) {
+     
+      if(multiple % i !== 0){
+        multiple += max;
+     
+     } 
+    
+  }
+
+  return multiple
+ 
+}
+ 
+  
+
+
+
+smallestCommons([1,5]);
